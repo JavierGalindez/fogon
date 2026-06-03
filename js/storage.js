67b7e72614ebcee7692f681dd54b32b7914b777f
Javier <javierguaca@unicomfacauca.edu.co>
2026-06-02 23:32:02 -0500

    fix dockerfile

--- js/storage.js
+++ js/storage.js
@@ -6,7 +6,7 @@ const Storage = (() => {
     }
     var h = window.location.hostname;
     if (h === 'localhost' || h === '127.0.0.1') {
-      return '/Proyecto_Final/api.php';
+      return '/api.php';
     }
     return '/api.php';
   };
